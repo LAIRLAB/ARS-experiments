6dbc01548ab9ff1fbf74d2847a9539297513e3f5
Anirudh Vemula <vvanirudh@gmail.com>
2018-04-12 18:44:56 -0400

    MNIST Gym env done. MNIST supervised and reinforce working. MNIST ARS remaining

---
+++ envs/mnist/mnist.py
@@ -0,0 +1,56 @@
+import gym
+import torch
+from torchvision import datasets, transforms
+import numpy as np
+
+
+class MNIST(gym.Env):
+    def __init__(self, train_batch_size, test_batch_size):
+        super(MNIST, self).__init__()
+        self.train_data = datasets.MNIST('data', train=True, download=True,
+                                    transform=transforms.Compose([
+                                        transforms.ToTensor(),
+                                        transforms.Normalize((0.1307,), (0.3081,))
+                                    ]))
+        self.test_data = datasets.MNIST('data', train=False, download=True,
+                                    transform=transforms.Compose([
+                                        transforms.ToTensor(),
+                                        transforms.Normalize((0.1307,), (0.3081,))
+                                    ]))
+        self.num_accesses = 0
+        self.train_length = len(self.train_data)
+        self.test_length = len(self.test_data)
+        self.train_batch_size = train_batch_size
+        self.test_batch_size = test_batch_size
+        self.reset()
+
+    def step(self, actions, test=False):
+        if not test:
+            self.num_accesses += self.train_batch_size
+
+        rewards = actions == self.y
+        rewards = rewards.type(torch.FloatTensor)
+        rewards = 2*rewards - 1
+
+        return None, rewards, None, None
+
+
+    def reset(self, test=False):
+        if test:
+            test_ind = torch.LongTensor(np.random.randint(self.test_length, size=self.test_batch_size))            
+            self.x = self.test_data.test_data.index_select(0, test_ind).type(torch.FloatTensor)
+            self.y = self.test_data.test_labels.index_select(0, test_ind)# .type(torch.FloatTensor)
+
+            return self.x, self.y
+        else:
+            train_ind = torch.LongTensor(np.random.randint(self.train_length, size=self.train_batch_size))
+            self.x = self.train_data.train_data.index_select(0, train_ind).type(torch.FloatTensor)
+            self.y = self.train_data.train_labels.index_select(0, train_ind)# .type(torch.FloatTensor)
+
+            return self.x, self.y
+
+    def get_num_accesses(self):
+        return self.num_accesses
+
+    def increment_accesses(self):
+        self.num_accesses += self.train_batch_size
---
+++ supervised/mnist_sgd.py
@@ -0,0 +1,91 @@
+from __future__ import print_function
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+from envs.mnist.mnist import MNIST
+import ipdb
+
+# Training settings
+parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                    help='input batch size for training (default: 64)')
+parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                    help='input batch size for testing (default: 1000)')
+parser.add_argument('--tsteps', type=int, default=100, metavar='N',
+                    help='number of epochs to train (default: 10)')
+parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+                    help='learning rate (default: 0.01)')
+parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
+                    help='SGD momentum (default: 0.5)')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='disables CUDA training')
+parser.add_argument('--seed', type=int, default=1, metavar='S',
+                    help='random seed (default: 1)')
+parser.add_argument('--log-interval', type=int, default=50, metavar='N',
+                    help='how many batches to wait before logging training status')
+args = parser.parse_args()
+args.cuda = not args.no_cuda and torch.cuda.is_available()
+
+torch.manual_seed(args.seed)
+if args.cuda:
+    torch.cuda.manual_seed(args.seed)
+
+env = MNIST(args.batch_size, args.test_batch_size)
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        # self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        # x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = F.relu(F.max_pool2d(self.conv2(x), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+model = Net()
+if args.cuda:
+    model.cuda()
+
+optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+
+for t in range(args.tsteps):
+    # Training
+    model.train()
+    x, y = env.reset()
+    x = x.view(args.batch_size, 1, 28, 28)
+    if args.cuda:
+        x, y = x.cuda(), y.cuda()
+    x, y = Variable(x), Variable(y)
+    optimizer.zero_grad()
+    output = model(x)
+    env.increment_accesses()
+    loss = F.nll_loss(output, y)
+    loss.backward()
+    optimizer.step()
+
+
+    # Test
+    model.eval()
+    x, y = env.reset(test=True)
+    x = x.view(args.test_batch_size, 1, 28, 28)
+    if args.cuda:
+        x, y = x.cuda(), y.cuda()
+    x, y = Variable(x, volatile=True), Variable(y, volatile=True)
+    output = model(x)
+    pred = output.data.max(1, keepdim=True)[1]
+    correct = pred.eq(y.data.view_as(pred)).long().sum()
+    accuracy = correct / args.test_batch_size
+    print(env.get_num_accesses(), accuracy)
