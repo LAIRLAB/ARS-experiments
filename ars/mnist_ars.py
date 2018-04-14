dd4a664d79036027f1179924dd839b5a066a3ad7
Anirudh Vemula <vvanirudh@gmail.com>
2018-04-13 21:10:20 -0400

    Setting tuned hyperparams for MNIST ARS

--- ars/mnist_ars.py
+++ ars/mnist_ars.py
@@ -31,10 +31,10 @@ parser.add_argument('--seed', type=int, default=1, metavar='S',
 parser.add_argument('--log-interval', type=int, default=50, metavar='N',
                     help='how many batches to wait before logging training status')
 # ARS params
-parser.add_argument('--stepsize', type=float, default=0.001, help='Stepsize for ARS')
-parser.add_argument('--num_directions', type=int, default=100, help='Number of directions sampled for ARS')
-parser.add_argument('--num_top_directions', type=int, default=10, help='Number of top direction used for ARS')
-parser.add_argument('--perturbation_length', type=float, default=0.01, help='Perturbation length for ARS')
+parser.add_argument('--stepsize', type=float, default=0.005, help='Stepsize for ARS')
+parser.add_argument('--num_directions', type=int, default=10, help='Number of directions sampled for ARS')
+parser.add_argument('--num_top_directions', type=int, default=5, help='Number of top direction used for ARS')
+parser.add_argument('--perturbation_length', type=float, default=0.1, help='Perturbation length for ARS')
 
 # Hyperparam tuning params
 parser.add_argument('--exp', action='store_true')
