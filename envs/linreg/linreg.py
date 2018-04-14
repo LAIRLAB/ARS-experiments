fixed the bug, gradient ascent instead of gradient descent

envs/linreg/linreg.py
@@ -25,7 +25,7 @@ class LinReg(gym.Env):
         self.x = np.random.randn(self.batch_size, self.input_dim+1)
         # Bias term should be 1
         self.x[:, 0] = 1
-        self.y = self.x.dot(self.w) + np.random.randn()*0.01
+        self.y = self.x.dot(self.w) + np.random.randn()*0.001
         return self.x, self.y
 
     def get_num_accesses(self):

reinforce/lin_reinforce.py
@@ -6,14 +6,14 @@ import ipdb
 from utils.adam import Adam
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--n_accesses', type=int, default=100000)
+parser.add_argument('--n_accesses', type=int, default=1000000)
 parser.add_argument('--tsteps', type=int, default=100)
-parser.add_argument('--lr', type=float, default=0.001)
+parser.add_argument('--lr', type=float, default=0.01)
 parser.add_argument('--momentum', type=float, default=0.5)
 parser.add_argument('--seed', type=int, default=1)
 parser.add_argument('--input_dim', type=int, default=100)
 parser.add_argument('--test_interval', type=int, default=10)
-parser.add_argument('--batch_size', type=int, default=100)
+parser.add_argument('--batch_size', type=int, default=64)
 
 args = parser.parse_args()
 
@@ -23,7 +23,7 @@ random.seed(args.seed)
 env = LinReg(args.input_dim, args.batch_size)
 optim = Adam(args.input_dim+1, args.lr)
 
-w = np.random.randn(args.input_dim+1) / np.sqrt(args.input_dim+1)
+w = 5*np.random.randn(args.input_dim+1) / np.sqrt(args.input_dim+1)
 
 g = open('linear-reinforce-'+str(args.seed)+'-'+str(args.input_dim)+'.csv', 'w')
 while True:    
@@ -32,18 +32,20 @@ while True:
     x, y = env.reset()
     pred = x.dot(w)
     mu_yhat = pred
-    std_yhat = 0.01
+    std_yhat = 0.1 #0.01
     yhat = np.random.normal(mu_yhat, std_yhat)
     _, reward, _, _ = env.step(yhat)
     mse_loss = -np.mean(reward)
-    grad = (1./args.batch_size) * (x.T.dot(reward*(yhat - mu_yhat))) * (1./(std_yhat**2))
-    w = optim.update(w, grad)
+    grad = (1./args.batch_size) * (x.T.dot(reward*(yhat - mu_yhat))) #* (1./(std_yhat**2))
+    w = optim.update(w, -grad)
 
     # Test
     x, y = env.reset()
     yhat = x.dot(w)
     _, reward, _, _ = env.step(yhat, test=True)
     mse_loss = -np.mean(reward)
+
+    print "mse_loss: {}".format(mse_loss)
         
     # print(env.get_num_accesses(), loss)
     g.write(str(env.get_num_accesses())+','+str(mse_loss)+'\n')