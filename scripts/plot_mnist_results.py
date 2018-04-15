87069ceaa2fa1865cebdab89d95dc3b366a2363d
Anirudh Vemula <vvanirudh@gmail.com>
2018-04-15 18:14:15 -0400

    Capping error bars at 1

--- scripts/plot_mnist_results.py
+++ scripts/plot_mnist_results.py
@@ -3,8 +3,8 @@ import csv
 import numpy as np
 
 seeds = [5488, 7151, 6027, 5448, 4236, 6458, 4375, 8917, 9636, 3834]
-# exps = ['ars', 'reinforce', 'sgd', 'naturalreinforce', 'newton']
-exps = ['ars', 'reinforce', 'sgd']
+# exps = ['ars', 'reinforce', 'sgd']
+exps = ['reinforce', 'sgd']
 num_accesses = 1000000
 
 n_accesses = {}
@@ -42,7 +42,7 @@ for e in exps:
 colors = ['red', 'blue', 'green', 'cyan']
 for i, e in enumerate(exps):
     plt.plot(n_accesses[e][5488], meanresults[e], color=colors[i], label=e)
-    plt.fill_between(n_accesses[e][5488], meanresults[e]-stdresults[e], meanresults[e]+stdresults[e], facecolor=colors[i], alpha=0.2)
+    plt.fill_between(n_accesses[e][5488], meanresults[e]-stdresults[e], np.minimum(1, meanresults[e]+stdresults[e]), facecolor=colors[i], alpha=0.2)
 
 plt.xlabel('Number of samples')
 plt.ylabel('Test accuracy')
