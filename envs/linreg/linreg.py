8bd041848720694931a031a8ca81f51d284ddb3f
Wen Sun <wensun@Wens-MBP.fios-router.home>
2018-04-15 11:55:38 -0400

    two updates: x sampled from a nonidentity and scaled cov matrix, x is normalized so that the max norm of x is independent on the dimension

--- envs/linreg/linreg.py
+++ envs/linreg/linreg.py
@@ -11,6 +11,14 @@ class LinReg(gym.Env):
         self.test_batch_size = test_batch_size
         self.w = np.random.randn(self.input_dim+1) / np.sqrt(self.input_dim+1)
         self.num_accesses = 0
+
+        cov = np.random.randn(self.input_dim+1, self.input_dim+1)
+        self.cov = cov.T.dot(cov)
+        s,U = np.linalg.eig(self.cov)
+        s[0:int(self.input_dim/2.)] *= 2.
+        s[int(self.input_dim/2.):] /= 2.
+        self.cov = U.dot(np.diag(s)).dot(U.T)
+
         self.reset()
 
     def step(self, yhat, test=False):
@@ -24,9 +32,14 @@ class LinReg(gym.Env):
 
     def reset(self, test=False):
         if not test:            
-            self.x = np.random.randn(self.batch_size, self.input_dim+1)
+            #self.x = np.random.randn(self.batch_size, self.input_dim+1)
+            self.x = np.random.multivariate_normal(np.zeros(self.input_dim+1), self.cov, self.batch_size)
+            self.x /= np.sqrt(self.input_dim)
         else:
-            self.x = np.random.randn(self.test_batch_size, self.input_dim+1)
+            #self.x = np.random.randn(self.test_batch_size, self.input_dim+1)
+            self.x = np.random.multivariate_normal(np.zeros(self.input_dim+1), self.cov, self.batch_size)
+            self.x /= np.sqrt(self.input_dim)
+            
         # Bias term should be 1
         self.x[:, 0] = 1
         self.y = self.x.dot(self.w) + np.random.randn()*0.001
