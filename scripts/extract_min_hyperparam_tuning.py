b108d00a162f97f44dff8731049623ae0c5a1782
Anirudh Vemula <vvanirudh@gmail.com>
2018-04-15 22:39:57 -0400

    Averaging over 3 random seeds for tuning

--- scripts/extract_min_hyperparam_tuning.py
+++ scripts/extract_min_hyperparam_tuning.py
@@ -1,20 +1,32 @@
 import numpy as np
 import csv
 
-filename='data/hyperparam_tuning_results'
-with open(filename, 'r') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',')
-    min_loss = np.inf
-    stepsize = None
-    numdirections = None
-    numtopdirections = None
-    perturbationlength = None
-    for row in reader:
-        if float(row[4]) < min_loss:
-            min_loss = float(row[4])
-            stepsize = float(row[0])
-            numdirections = int(row[1])
-            numtopdirections = int(row[2])
-            perturbationlength = float(row[3])
+seeds = [283, 457, 623]
+losses = np.zeros(525)
+stepsizes_arr = np.zeros(525)
+numdirections_arr = np.zeros(525)
+numtopdirections_arr = np.zeros(525)
+pertlength_arr = np.zeros(525)
+for s in seeds:
+    filename='data/hyperparam_tuning_results_'+str(s)
+    with open(filename, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        e = 0
+        for row in reader:
+            losses[e] += float(row[4])
+            stepsizes_arr[e] = float(row[0])
+            numdirections_arr[e] = float(row[1])
+            numtopdirections_arr[e] = float(row[2])
+            pertlength_arr[e] = float(row[3])
+            e += 1
+        
+losses = losses / 3.
+
+ind = np.argmin(losses)
+stepsize = stepsizes_arr[ind]
+numdirections = numdirections_arr[ind]
+numtopdirections = numtopdirections_arr[ind]
+perturbationlength = pertlength_arr[ind]
+min_loss = losses[ind]
 
 print('Stepsize:', stepsize, 'Num Directions:', numdirections, 'Num Top Directions:', numtopdirections, 'Perturbation Length:', perturbationlength, 'Loss:', min_loss)
