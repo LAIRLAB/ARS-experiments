53f26ef59343503a0a87c167d62941f3ff84f458
Anirudh Vemula <vvanirudh@gmail.com>
2018-04-17 22:49:49 -0400

    Decreased number of accesses required in ars script

--- scripts/run_linear_ars.sh
+++ scripts/run_linear_ars.sh
@@ -4,5 +4,5 @@ seeds=(5488 7151 6027 5448 4236 6458 4375 8917 9636 3834)
 
 for s in ${seeds[@]}
 do
-    python -m ars.lin_ars --seed=$s --input_dim=$1 --n_accesses=1000000
+    python -m ars.lin_ars --seed=$s --input_dim=$1 --n_accesses=300000
 done
