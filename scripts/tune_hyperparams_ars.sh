6f95c7439de4000506f37ce5ccf623b6da20aaab
Anirudh Vemula <vvanirudh@gmail.com>
2018-04-27 13:42:12 -0400

    New tuning script for ARS

--- scripts/tune_hyperparams_ars.sh
+++ scripts/tune_hyperparams_ars.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-stepsizes=(0.01 0.02 0.03)
-numdirections=(50 100 200)
+stepsizes=(0.001 0.005 0.01 0.02 0.03)
+numdirections=(10 50 100 200 500)
 # numdirections=(50)
-pertubationlengths=(0.01 0.02 0.03)
-numtopdirections=(20 50 100)
+pertubationlengths=(0.001 0.005 0.01 0.02 0.03)
+numtopdirections=(5 10 50 100 200)
 seeds=(283 457 623)
 
 for s in ${stepsizes[@]}
@@ -20,7 +20,7 @@ do
         else
 	    for sd in ${seeds[@]}
 	    do
-		python -m ars.lin_ars --exp --stepsize=$s --num_directions=$nd --perturbation_length=$pl --num_top_directions=$ntd --seed=$sd &
+		python -m ars.lin_ars --exp --stepsize=$s --num_directions=$nd --perturbation_length=$pl --num_top_directions=$ntd --seed=$sd --input_dim=100 --threshold=100000 &
 	    done
 	    wait
         fi
