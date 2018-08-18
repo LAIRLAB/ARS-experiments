e580d90d9c01d2141defc8a8e53331f77075b3c9
Anirudh Vemula <vvanirudh@gmail.com>
2018-08-18 17:19:19 -0400

    Adding argument support

--- ars/run_ars_lqr_script.py
+++ ars/run_ars_lqr_script.py
@@ -2,6 +2,14 @@ import numpy as np
 from ars.lqr_ars import *
 from envs.LQR.LQR import LQREnv
 import pickle
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--exp', action="store_true", help="Tag required to specify horizon lengths")
+parser.add_argument('--H_start', type=int, default=10, help="Horizon length to start with")
+parser.add_argument('--H_end', type=int, default=200, help="Horizon length to end with")
+parser.add_argument('--H_bin', type=int, default=20, help="Horizon length spacing at which experiments are done (or bin size)")
+args = parser.parse_args()
 
 print ("start running ars experiment")
 # ss = {10: 0.01, 20: 0.01, 40: 0.005, 60: 0.005, 80: 0.01, 100: 0.01, 120: 0.01, 140: 0.01, 160: 0.01}
@@ -19,7 +27,10 @@ test_param_seed = np.random.randint(low = 1, high = 1e8, size = 10)
 x_dim = 100 #500
 a_dim = 1
 
-Hs = [5, 10, 20, 40, 60, 80, 100, 120, 140, 160, 180, 200]
+if args.exp:
+    Hs = list(range(args.H_start, args.H_end + args.H_bin, args.H_bin))
+else:    
+    Hs = [5, 10, 20, 40, 60, 80, 100, 120, 140, 160, 180, 200]
 
 
 K0 = np.ones((a_dim, x_dim))*0.01
@@ -36,4 +47,8 @@ for H in Hs:
 
     test_perf_cross_H.append(test_perf_seeds)
 
-pickle.dump(test_perf_cross_H, open("ars_result_cross_H_10_160.p", "wb"))
+if args.exp:    
+    filename = "ars_result_cross_H_" + str(args.H_start)+"_"+str(args.H_end)+"_"+str(args.H_bin)+".p"
+else:
+    filename = "ars_result_cross_H_10_160.p"    
+pickle.dump(test_perf_cross_H, open(filename, "wb"))
--- scripts/plot_lqr_results.py
+++ scripts/plot_lqr_results.py
@@ -1,20 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pickle
+import argparse
 
-ars_results = pickle.load(open('ars_result_cross_H_10_160.p', 'rb'))
-exact_results = pickle.load(open('exact_result_cross_H_10_160.p', 'rb'))
-reinforce_results = pickle.load(open('lqr_reinforce_cross_H_10_160.p', 'rb'))
+parser = argparse.ArgumentParser()
+parser.add_argument('--H_start', type=int, default=10, help="Horizon length to start with")
+parser.add_argument('--H_end', type=int, default=200, help="Horizon length to end with")
+parser.add_argument('--H_bin', type=int, default=20, help="Horizon length spacing at which experiments are done (or bin size)")
+args = parser.parse_args()
+
+ars_filename = "ars_result_cross_H_" + str(args.H_start)+"_"+str(args.H_end)+"_"+str(args.H_bin)+".p"
+exact_filename = "exact_result_cross_H_" + str(args.H_start)+"_"+str(args.H_end)+"_"+str(args.H_bin)+".p"
+
+ars_results = pickle.load(open(ars_filename, 'rb'))
+exact_results = pickle.load(open(exact_filename, 'rb'))
+# reinforce_results = pickle.load(open(args.reinforce_data, 'rb'))
 
 mean_ars = np.mean(np.array(ars_results), axis=1)
 mean_exact = np.mean(np.array(exact_results), axis=1)
-mean_reinforce = np.mean(np.array(reinforce_results), axis=1)
+# mean_reinforce = np.mean(np.array(reinforce_results), axis=1)
 
 std_ars = np.std(np.array(ars_results), axis=1)
 std_exact = np.std(np.array(exact_results), axis=1)
-std_reinforce = np.std(np.array(reinforce_results), axis=1)
+# std_reinforce = np.std(np.array(reinforce_results), axis=1)
 
-H = [10, 20, 40, 60, 80, 100, 120, 140, 160]
+# H = [10, 20, 40, 60, 80, 100, 120, 140, 160]
+H = list(range(args.H_start, args.H_end+args.H_bin, args.H_bin))
 
 # mean_ars = np.divide(mean_ars, H)
 # std_ars = np.divide(std_ars, H)
@@ -31,7 +42,9 @@ plt.fill_between(H, np.maximum(0, mean_ars - std_ars/2), np.minimum(1e6, mean_ar
 plt.plot(H, mean_exact, color='blue', label='EXACT', linewidth=2)
 plt.fill_between(H, np.maximum(0, mean_exact - std_exact/2), np.minimum(1e6, mean_exact + std_exact/2), facecolor='blue', alpha=0.2)
 
-plt.plot(H, mean_reinforce, color='green', label='REINFORCE', linewidth=2)
-plt.fill_between(H, np.maximum(0, mean_reinforce - std_reinforce/2), np.minimum(1e6, mean_reinforce + std_reinforce/2), facecolor='green', alpha=0.2)
+# plt.plot(H, mean_reinforce, color='green', label='REINFORCE', linewidth=2)
+# plt.fill_between(H, np.maximum(0, mean_reinforce - std_reinforce/2), np.minimum(1e6, mean_reinforce + std_reinforce/2), facecolor='green', alpha=0.2)
+
+plt.legend()
 
 plt.show()
