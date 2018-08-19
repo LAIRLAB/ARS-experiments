2e216fbb46af7702725958bc94210a67147b49e5
Anirudh Vemula <vvanirudh@gmail.com>
2018-08-19 12:51:34 -0400

    Modified run script for LQR reinforce

--- reinforce/run_lqr_reinforce_script.py
+++ reinforce/run_lqr_reinforce_script.py
@@ -13,6 +13,10 @@ parser.add_argument('--H_end', type=int, default=200, help="Horizon length to en
 parser.add_argument('--H_bin', type=int, default=20, help="Horizon length spacing at which experiments are done (or bin size)")
 args = parser.parse_args()
 
+filename = "tune_lqr_reinforce_H_" + str(args.H_start)+"_"+str(args.H_end)+"_"+str(args.H_bin)+".p"
+_, data = pickle.load(open(filename, 'rb'))
+lr, explore_mag = data
+
 initial_seed=1000
 np.random.seed(initial_seed)
 num_random_seeds = 10
@@ -20,15 +24,12 @@ test_param_seed = np.random.randint(low = 1, high = 1e8, size = num_random_seeds
 x_dim = 100
 a_dim = 1
 
-
 Hs = list(range(args.H_start, args.H_end + args.H_bin, args.H_bin))
 
-
-lr = 0.005 #0.01
 K0 = np.ones((a_dim, x_dim))*0.01
 test_perf_cross_H = []
 bar = Bar('Processing', max = len(Hs) * num_random_seeds)
-for H in Hs:
+for H_id, H in enumerate(Hs):
     #print("at H = {0}".format(H))    
     test_perf_seeds = []
     for seed in test_param_seed:
@@ -36,14 +37,14 @@ for H in Hs:
         bar.next()
         np.random.seed(seed)
         random.seed(seed)
-        optimizer = Adam(x_dim*a_dim+1, lr)
+        optimizer = Adam(x_dim*a_dim+1, lr[H_id])
         env = LQREnv(x_dim = x_dim, u_dim = a_dim, rank = 5, seed=seed, T = H)
         batch_size = 100 # 10*H
         max_iter = 1e5 * H
-        steps = policy_gradient_adam_linear_policy(env,explore_mag=0.1,
+        steps = policy_gradient_adam_linear_policy(env,explore_mag=explore_mag[H_id],
                                             optimizer = optimizer, batch_size=batch_size,
                                             max_iter = max_iter,
-                                            K0=K0, Natural=False, kl=0.005, stats=None)
+                                                   K0=K0, Natural=False, kl=0.005, stats=None, verbose=False)
         test_perf_seeds.append(steps)
 
     test_perf_cross_H.append(test_perf_seeds)
