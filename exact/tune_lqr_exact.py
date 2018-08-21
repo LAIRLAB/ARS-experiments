cbe265aee08aff19a4b524fbd259e5959917acc3
Anirudh Vemula <vvanirudh@gmail.com>
2018-08-20 21:35:16 -0400

    Back to original tuning

--- exact/tune_lqr_exact.py
+++ exact/tune_lqr_exact.py
@@ -15,9 +15,9 @@ args = parser.parse_args()
 
 print("start tuning parameters for exact")
 stepsize = [0.0005, 0.001, 0.005, 0.01]
-num_directions = [1, 10, 20, 50]
-num_top_directions = [1, 5, 10, 20, 50]
-pertubation = [0.0005, 0.001, 0.005, 0.01]
+num_directions = [10, 20, 50]
+num_top_directions = [5, 10, 20, 50]
+pertubation = [0.001, 0.005, 0.01]
 horizons = list(range(args.H_start, args.H_end + args.H_bin, args.H_bin))
 result_table = [np.zeros((len(stepsize), len(num_directions), len(num_top_directions), len(pertubation))) for _ in range(len(horizons))]
 
